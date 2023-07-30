bff275b01075a6fafbfde3e044958ea3a3937438
Usama1100 <usamaiftikhar59@gmail.com>
2023-07-30 11:50:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Model1 from './components/Model1';
+import Model2 from './components/Model2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: '50vh' , backgroundColor: 'black'}} />
+      <Model1 />
+      <div style={{ height: '55vh' , backgroundColor: 'black'}} />
+      <Model2 />
+      <div style={{ height: '50vh' , backgroundColor: 'black'}} />
     </div>
   );
 }
 
 export default App;
+
